d09fe3a6fd847c05debedd70f7872a7f7b8a36bf
Nixcandiales <61337676+Nixoncandiales@users.noreply.github.com>
2022-09-07 16:02:36 -0400

    Update the 3 databases
    Update the 3 data bases.
    
    To do:
     *stcrosswalk !!! Double check.  to link states across all 3 data bases

---
+++ Assigments/AS 1/Code/ACA/3_Build.r
@@ -0,0 +1,25 @@
+# Meta --------------------------------------------------------------------
+
+## Title:  Combine ACS and Medicaid Expansion Data
+## Author: Ian McCarthy
+## Date Created: 12/6/2019
+## Date Edited:  4/2/2020
+
+
+# Preliminaries -----------------------------------------------------------
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, readr, readxl, scales, acs, tidyr, here)
+
+source(here("Assigments", "AS 1", "Code","ACA","1_download_CENSUS_ACA.r"))
+source(here("Assigments", "AS 1", "Code","ACA","2_Medicaid.r"))
+
+
+# Tidy --------------------------------------------------------------------
+final.data <- final.insurance %>%
+  left_join(kff.final, by="State") %>%
+  mutate(expand_year = year(date_adopted),
+         expand = (year>=expand_year & !is.na(expand_year))) %>%
+  rename(expand_ever=expanded)
+
+
+write_tsv(final.data,here("Assigments", "AS 1", "Output","ACA","acs_medicaid.txt"))
