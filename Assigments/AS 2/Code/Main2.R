a6621984bab42de3510cc897044c9d6aeee62254
Nixoncandiales <nixon.candiales@utexas.edu>
2022-10-13 01:22:46 -0400

    Update Main2.R

--- Assigments/AS 2/Code/Main2.R
+++ Assigments/AS 2/Code/Main2.R
@@ -155,21 +155,20 @@ gc()
 ### Re do this!!!!!! program and use FEOLS. That command actually works on my machine yujuuu!
 
 
-## ----instrument, eval=FALSE, echo=TRUE-------------------------------------------------------------------------------
-## #source(here("Code", "Instrument.R"))
-## 
-## # Append the data
-## reg.dat.2sls <- left_join(reg.dat, price.shock, by=c("group1" = "tax_id", "Year" = "Year"))
-## 
-## #mod.2sls <- ivreg(log_y ~ average_submitted_chrg_amt + average_medicare_payment_amt + factor(npi) + factor(Year) | int | practice_rev_change, data = reg.dat.2sls) ## To computational heavy
-## 
-## mod.2sls.plm <- plm(log_y ~ int + average_submitted_chrg_amt + average_medicare_payment_amt  | average_submitted_chrg_amt + average_medicare_payment_amt + practice_rev_change, model = "within", effect = "twoways", index = c("npi","Year"), data = reg.dat.2sls)
-## modelsummary(mod.2sls.plm, output = "modelsummary_list", stars = TRUE)
-## 
-## mod.2sls.feols <- feols(log_y ~ average_submitted_chrg_amt + average_medicare_payment_amt  | npi + Year | int ~ practice_rev_change, data = reg.dat.2sls)
-## mod.2sls <- modelsummary(summary(mod.2sls.feols, stage = 1:2),stars = TRUE, output = "modelsummary_list")
-## 
-## ## Both plm and feols give me the same coeff, different se
+## ----Q5--------------------------------------------------------------------------------------------------------------
+#source(here("Code", "Instrument.R"))
+
+# Append the data
+reg.dat.2sls <- left_join(reg.dat, price.shock, by=c("group1" = "tax_id", "Year" = "Year"))
+
+#mod.2sls <- ivreg(log_y ~ average_submitted_chrg_amt + average_medicare_payment_amt + factor(npi) + factor(Year) | int | practice_rev_change, data = reg.dat.2sls) ## Too computational ineficient
+
+mod.2sls.plm <- plm(log_y ~ int + average_submitted_chrg_amt + average_medicare_payment_amt  | average_submitted_chrg_amt + average_medicare_payment_amt + practice_rev_change, model = "within", effect = "twoways", index = c("npi","Year"), data = reg.dat.2sls)
+modelsummary(mod.2sls.plm, output = "modelsummary_list", stars = TRUE)
+
+mod.2sls.feols <- feols(log_y ~ average_submitted_chrg_amt + average_medicare_payment_amt  | npi + Year | int ~ practice_rev_change, data = reg.dat.2sls)
+mod.2sls <- modelsummary(summary(mod.2sls.feols, stage = 1:2),stars = TRUE, output = "modelsummary_list")
+# Both plm and feols give me the same coeff, different se
 
 
 ## ----Q6--------------------------------------------------------------------------------------------------------------
