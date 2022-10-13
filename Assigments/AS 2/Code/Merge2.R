a72760fd3578341abc4543f57b2e5c1af7e8b7c7
Nixcandiales <61337676+Nixoncandiales@users.noreply.github.com>
2022-10-12 21:07:22 -0400

    Q5 - instrument

--- Assigments/AS 2/Code/Merge2.R
+++ Assigments/AS 2/Code/Merge2.R
@@ -23,6 +23,15 @@ list.files(path = paste0("/Users/nix/Documents/GitHub/Econ771/Assigments/AS 2/Da
            full.names = TRUE) -> dir
 
 #------
+#----- data bases needed for the instrument, keep them out of the loop
+#------
+
+    taxid.base <- vroom(here("Data", "PhysicianData_2009.csv")) %>% 
+                  select(npi, tax_id_2009 = group1)
+
+    pfs <- vroom(here("Data", "pfs.txt")) 
+
+#------
 #----- For loop to read the data in iterations by each year, clean and append
 #------
 
@@ -43,6 +52,7 @@ for (i in 2012:2017) {
     group_by(Year, npi) %>%
     mutate(
       int = ifelse( pos_opd / (pos_opd + pos_office + pos_asc) >= 0.75,1,0), #Create int variable for Q2
+      tax_id = group1 + group2   
     ) %>%
     select(Year,npi,int, group1, group2)
   
@@ -59,18 +69,39 @@ for (i in 2012:2017) {
               Total_Spending= average_medicare_allowed_amt*line_srvc_cnt, 
               Total_Claims= line_srvc_cnt, 
               Total_Patients= bene_unique_cnt
-            ) %>%
-            group_by(npi) %>%
-            summarise(
-                  Total_Spending                = sum(Total_Spending, na.rm = TRUE),
-                  Total_Claims                  = sum(Total_Claims, na.rm = TRUE),
-                  Total_Patients                = sum(Total_Patients, na.rm = TRUE),
-                  average_submitted_chrg_amt    = sum(average_submitted_chrg_amt, na.rm = TRUE),
-                  average_medicare_payment_amt  = sum(average_medicare_payment_amt, na.rm = TRUE),
-                  average_medicare_allowed_amt  = sum(average_medicare_allowed_amt, na.rm = TRUE),
-                  line_srvc_cnt                 = sum(line_srvc_cnt, na.rm = TRUE),
-                  bene_unique_cnt               = sum(bene_unique_cnt, na.rm = TRUE)
-                )
+            ) #%>%
+#            group_by(npi) %>%
+#            summarise(
+#                  Total_Spending                = sum(Total_Spending, na.rm = TRUE),
+#                  Total_Claims                  = sum(Total_Claims, na.rm = TRUE),
+#                  Total_Patients                = sum(Total_Patients, na.rm = TRUE),
+#                  average_submitted_chrg_amt    = sum(average_submitted_chrg_amt, na.rm = TRUE),
+#                  average_medicare_payment_amt  = sum(average_medicare_payment_amt, na.rm = TRUE),
+#                  average_medicare_allowed_amt  = sum(average_medicare_allowed_amt, na.rm = TRUE),
+#                  line_srvc_cnt                 = sum(line_srvc_cnt, na.rm = TRUE),
+#                  bene_unique_cnt               = sum(bene_unique_cnt, na.rm = TRUE)
+#                )
+
+  #------
+  # Create the instrument for int -> price.shock
+  #------
+    price.shock <- b %>% inner_join(taxid.base, by="npi") %>%
+                 inner_join(pfs %>% filter(year == i) %>%
+                              select(hcpcs, dprice_rel_2010, price_nonfac_orig_2010, price_nonfac_orig_2007),
+                            by=c("hcpcs_code"="hcpcs")) %>%
+                 mutate_at(vars(dprice_rel_2010, price_nonfac_orig_2010, price_nonfac_orig_2007), replace_na, 0) %>%
+                 mutate(price_shock = case_when(
+                         i<=2013 ~ ((i-2009)/4)*dprice_rel_2010,
+                         i>2013  ~ dprice_rel_2010),
+                       denom = line_srvc_cnt*price_nonfac_orig_2010,
+                       numer = price_shock*line_srvc_cnt*price_nonfac_orig_2010) %>%
+                 group_by(npi) %>%
+                 summarize(phy_numer=sum(numer, na.rm=TRUE), phy_denom=sum(denom, na.rm=TRUE), tax_id=first(tax_id)) %>%
+                 ungroup() %>%
+                 mutate(phy_rev_change=phy_numer/phy_denom) %>%
+                 group_by(tax_id) %>%
+                 summarize(practice_rev_change=sum(phy_rev_change, na.rm=TRUE)) %>%
+                 ungroup()
 
   # Write the inner join of MDPPAS and PUF in Disk in rectangular form for Year i 
   # -> gives just the cases that we observe in both data sets
