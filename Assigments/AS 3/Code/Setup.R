bd0cd9a710fab0bc648f3b906c99986dfe6f1450
Nixcandiales <61337676+Nixoncandiales@users.noreply.github.com>
2022-10-25 14:21:50 -0400

    .

--- Assigments/AS 3/Code/Setup.R
+++ Assigments/AS 3/Code/Setup.R
@@ -1,1 +1,27 @@
-#This script will do the setup for the replication code.
+# Meta --------------------------------------------------------------------
+
+## Author:        Nixon Candiales
+## Date Created:  10/19/2022
+## Date Edited:   10/19/2022
+## Description:   Analysis file for empirical exercise 3
+
+# Preliminaries -----------------------------------------------------------
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, vroom, here, haven)
+
+source(here("Assigments", "AS 3", "Code", "set.mydir.R"))
+
+# Load the data -----------------------------------------------------------
+
+dat <- read_dta("Data/Data_main.dta")
+
+# Pat 1 replication -------------------------------------------------------
+dat %>% group_by(orgParentCode) %>% mutate(firmID = cur_group_id())
+
+dat %>% group_by(uniqueID) %>% mutate(uniqueIDNum = cur_group_id())
+
+prueba <- function(var, name){
+
+dat %>% group_by({var}) %>% transmute({name} = cur_group_id())
+
+}
---
+++ Assigments/AS 3/Code/set.mydir.R
@@ -0,0 +1,23 @@
+
+# Clean Memory
+rm(list=ls())
+gc()
+
+# Create a function to set the Working Directory no matter the os.
+set.mydir <- function(name="nixon") {
+
+    temp <- Sys.info()
+
+        if (temp['sysname']=="Windows"){
+            setwd("~/Github/Econ771/Assigments/AS 3")
+        #   here::i_am(Setmydir.R)
+            return(print(paste0("Hello ", temp['nodename'], 
+                                " you are in ", temp['sysname'], 
+                                " and your root directory is: ", getwd()
+                                )
+                        )
+                    )
+        }
+}
+
+set.mydir()
