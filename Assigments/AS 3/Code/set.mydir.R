214eda2c97e076f47b32302aa02a1ee24ccaf441
Nixoncandiales <nixon.candiales@utexas.edu>
2022-10-25 22:19:31 -0400

    update dir when working on mac

--- Assigments/AS 3/Code/set.mydir.R
+++ Assigments/AS 3/Code/set.mydir.R
@@ -4,10 +4,19 @@ rm(list=ls())
 gc()
 
 # Create a function to set the Working Directory no matter the os.
-set.mydir <- function(name="nixon") {
+set.mydir <- function(name="Nixon") {
 
     temp <- Sys.info()
 
+        if (temp['sysname']=="Darwin"){
+            setwd("/Users/nix/Documents/GitHub/Econ771/Assigments/AS 3")
+            return(print(paste0("Hello ", name, 
+                    " you are in ", temp['sysname'], 
+                    " and your root directory is: ", getwd()
+                                )
+                        )
+                   )
+        }
         if (temp['sysname']=="Windows"){
             setwd("~/Github/Econ771/Assigments/AS 3")
         #   here::i_am(Setmydir.R)
